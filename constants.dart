32e83ad3a8d09d63bd8370a3a7466ead4c9a0188
akmalshakir <akmalshakir0011@gmail.com>
2025-03-01 10:15:21 +0500

    constants.dart
    These are all the constants.

---
+++ constants.dart
@@ -0,0 +1,11 @@
+const String API_KEY = "";
+const String BASE_URL = "https://newsapi.org/v2";
+const List<String> categories = [
+  "technology",
+  "business",
+  "sports",
+  "entertainment",
+  "health"
+];
+
+
